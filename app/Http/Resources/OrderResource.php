242d04b447e4e04f07a781bc3ccdb4eaac111f40
michael <michaelwilliams01@protonmail.com>
2021-06-14 20:47:49 +0100

    Adding Resources for fetching collections of Orders and Order Itmes

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,6 +14,12 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'total' => $this->admin_revenue,
+            'order_items' => OrderItemResource::collection($this->whenLoaded('orderItems')),
+        ];
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\OrderItem;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,20 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    public function getNameAttribute()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    public function getAdminRevenueAttribute()
+    {
+        return $this->orderItems->sum(function(OrderItem $item ) {
+            return $item->admin_revenue;
+        });
+    }
 }
