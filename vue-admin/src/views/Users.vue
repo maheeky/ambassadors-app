e8accf6871d33e0863e461c4ac7daef5891fa6db
michael <michaelwilliams01@protonmail.com>
2021-06-16 21:24:54 +0100

    Pulling in user data from Ambassadors controller, displaying in Vuetify table

--- vue-admin/src/views/Users.vue
+++ vue-admin/src/views/Users.vue
@@ -1,34 +1,52 @@
 <template>
-    <div class="table-responsive">
-        <table class="table table-striped table-sm">
-        <thead>
-            <tr>
-            <th>#</th>
-            <th>Header</th>
-            <th>Header</th>
-            <th>Header</th>
-            <th>Header</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>1,001</td>
-            <td>random</td>
-            <td>data</td>
-            <td>placeholder</td>
-            <td>text</td>
-            </tr>
-        </tbody>
-        </table>
-    </div>
+
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead class="table-head">
+        <tr>
+            <th class="text-left">#</th>
+            <th class="text-left">Name</th>
+            <th class="text-left">Email</th>
+            <th class="text-left">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="user in users"
+          :key="user.id"
+        >
+            <td>{{user.id}}</td>
+            <td>{{user.first_name }}{{ user.last_name }}</td>
+            <td>{{user.email}}</td>
+            <td>
+                <v-btn :href="`/users/${user.id}/links`" color="primary" elevation="2">View</v-btn>
+            </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: 'Users'
+        name: 'Users',
+        data() {
+            return {
+                users: []
+            }
+        },
+        async mounted() {
+            const {data} = await axios.get('ambassadors');
+
+            this.users = data;
+        }
     }
 </script>
 
 <style scoped>
-
+    .table-head {
+        margin-top: 2.5em;
+    }
 </style>
